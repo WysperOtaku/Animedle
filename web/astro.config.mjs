0c538ce31c81958872a56518734d33f46b4d60a2
Eric Mejias Gamonal <ericmejiasgamonal@gmail.com>
2025-12-02 16:33:36 +0100

    Astro infraestructure

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,24 +1,49 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
+	devToolbar: {
+		enabled: false
+	},
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Animedle API Docs',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/wysperotaku/animedle' }
+			],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Como funciona',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Como funciona', slug: 'how-it-works' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Documentacion',
+					items: [
+						{ label: 'Reto diario', slug: 'docs/endpoints/daily-challenge' },
+						{ label: 'Historial', slug: 'docs/endpoints/history' },
+						{ label: 'Anime por ID', slug: 'docs/endpoints/anime-id' },
+					],
+				},
+				{
+					label: 'Ejemplos',
+					items: [
+						{ label: 'JavaScript', slug: 'docs/examples/javascript' },
+						{ label: 'TypeScript', slug: 'docs/examples/typescript' },
+					],
+				},
+				{
+					label: 'Juego de pruebas',
+					items: [
+						{ label: 'Juego de pruebas', slug: 'playground' },
+					],
+				},
+				{
+					label: 'Sobre el proyecto',
+					items: [
+						{ label: 'Sobre el proyecto', slug: 'about' },
+					],
 				},
 			],
 		}),
