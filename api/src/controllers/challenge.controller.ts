af8b153df3400dca51e62c0532a33e88331f0e41
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2026-01-21 13:35:40 +0100

    New responses for api endpoints

--- api/src/controllers/challenge.controller.ts
+++ api/src/controllers/challenge.controller.ts
@@ -1,23 +1,67 @@
 import { Request, Response, NextFunction } from "express";
+import { getRetosDaily } from "../services/challenge.service";
+import { stat } from "fs";
+import { parse } from "path";
 
-export const parseDaily = (req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json({
-        status: "failed",
-        message: "Not implemented daily"
-    });
+export const parseDaily = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await getRetosDaily();
+
+        const response = {
+            status: "success",
+            data: {
+                challenges: result
+            }
+        }
+
+        res.status(200).json({ response });
+    }
+    catch (error) {
+        return res.status(500).json({
+            status: "error",
+            message: (error as Error).message
+        });
+    }
+    
+    next();
 }
 
-export const parseHistory = (req: Request, res: Response, next: NextFunction) => {
-    const limit = req.query.limit;
-    res.status(200).json({
-        status: "success",
-        message: limit
-    });
+export const parseHistory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const limit = parseInt(req.query.limit as string);
+        if (isNaN(limit)) {
+            return res.status(400).json({
+                status: "failed",
+                message: "The limit value is not a number."
+            });
+        }
+
+        const result = "";
+
+        const response = {
+            status: "success",
+            data: {
+                challenges: result
+            }
+        }
+
+        res.status(200).json({ response });
+    }
+    catch (error) {
+        return res.status(500).json({
+            status: "error",
+            message: (error as Error).message
+        });
+    }
+
+    next();
 }
 
-export const parseDate = (req: Request, res: Response, next: NextFunction) => {
+export const parseDate = async (req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({
         status: "failed",
         message: "Not implemented date"
     });
+
+    next();
 }
