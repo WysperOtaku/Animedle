fc91e5e1188f4c9182b37824985afbd9c4765d41
Wysper <wysperotaku@gmail.com>
2026-01-25 21:07:18 +0100

    chore: remove uploads from git, add to .gitignore and use PORT env

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,8 @@
 import './utils/dotenv';
 import app from './app';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(
-    `Server listening in http://localhost:${port}`
-  );
+  console.log(`Server listening on port ${port}`);
 });
